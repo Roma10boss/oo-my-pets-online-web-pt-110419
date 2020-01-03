5577aa195b9d6eff11b6fa01f7a3c50a0e67307d
Romario Gustave <romagustave10@gmail.com>
2020-01-03 22:14:35 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,19 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name 
+  
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner
+    @mood ="nervous"
+    save 
+  end 
+  
+  def save 
+    @@cats << self 
+  end 
+  
+  def sel.all 
+    @@cats 
+  end 
 end
