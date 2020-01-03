08ffc8beb50f38bb17650b0eff5e728a0351f37e
Romario Gustave <romagustave10@gmail.com>
2020-01-03 22:05:13 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,7 +4,41 @@ class Owner
   attr_reader :name, :species 
   @@all_owner =[]
   
-  def initialize(name,species)
-    @name
+  def initialize(name,species="human")
+    @name = name
+    @species = species
+    save 
   end 
+  
+  def save 
+    @@all_owner << self 
+  end 
+  
+  def say_species 
+    "I am a #{species}."
+  end 
+  
+  dwf self.all_owner
+  @@all_owner
+end 
+
+def self.count 
+  self.all.length 
+end 
+
+def self.reset_all 
+  self.all.clear
+end 
+
+def add_cat(cat)
+  cat.owner = self 
+end 
+
+def add_dog(dog) 
+  dog.owner = self 
+end 
+
+def cats 
+  Cat.all.select {|cat| cat.owner == self 
+  
 end
