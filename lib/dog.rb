a9a76ac72fa7e8ad8d1972051f7eda31905e83c4
Romario Gustave <romagustave10@gmail.com>
2020-01-03 22:18:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,21 @@
 class Dog
-  attr_accessor :owner, :mood
+  attr_accessor :owner, :mood 
   attr_reader :name 
+  @@dogs = [] 
   
   def initialize(name, owner)
     @name = name 
-    @owner = owner
-    @mood ="nervous"
+    @mood = "nervous" 
+    @owner = owner 
     save 
-  end 
+  end
   
   def save 
     @@dogs << self 
   end 
   
-  def sel.all 
-    @@dogs  
+  def self.all 
+    @@dogs 
   end 
+  
 end
